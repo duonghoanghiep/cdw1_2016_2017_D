390965e086a5209465a06160188e390c51e745b2
duonghoanghiep <duonghoanghiep895@gmail.com>
2017-03-15 17:16:55 +0700

    db

--- laravel/packages/foostart/contact/src/views/contact/elements/text.blade.php
+++ laravel/packages/foostart/contact/src/views/contact/elements/text.blade.php
@@ -1,11 +1,15 @@
 <head>
   <script src='//cloud.tinymce.com/stable/tinymce.min.js'></script>
+  
   <script>
   tinymce.init({
     selector: 'textarea',  // change this value according to your HTML
+    
   plugin: 'a_tinymce_plugin',
   a_plugin_option: true,
-  a_configuration_option: 400
+  a_configuration_option: 400,
+  
+  
   });
   </script>
 </head>
